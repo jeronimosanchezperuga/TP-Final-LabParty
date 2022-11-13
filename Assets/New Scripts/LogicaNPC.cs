cbe408316fc46baed8965e10fc1d3d0b479aca89
JoacoSV <joaquin.suarezvallejos@gmail.com>
2022-11-13 19:04:06 -0300

    Update V4

--- Assets/New Scripts/LogicaNPC.cs
+++ Assets/New Scripts/LogicaNPC.cs
@@ -19,6 +19,7 @@ public class LogicaNPC : MonoBehaviour
     public static bool check3 = false;
     public static bool check4 = false;
     public static bool checkMujer2 = false;
+    [SerializeField] NPC1Data NPCOddChap, NPCMujer1Data, NPCMujer2Data, NPCHombre1Data;
 
     void Start()
     {
@@ -29,6 +30,10 @@ public class LogicaNPC : MonoBehaviour
         check3 = false;
         check4 = false;
         checkMujer2 = false;
+        NPCOddChap.hasTalked = false;
+        NPCMujer1Data.hasTalked = false;
+        NPCMujer2Data.hasTalked = false;
+        NPCHombre1Data.hasTalked = false;
     }
 
     void OnTriggerEnter(Collider col)
@@ -53,35 +58,98 @@ public class LogicaNPC : MonoBehaviour
             }
         }
 
-        if (col.gameObject.tag == "NPC2")
+        if (NPCMujer2Data.hasTalked == false)
         {
-            inticon.SetActive(true);
-            panelNPCHablar.SetActive(true);
+            if (col.gameObject.tag == "NPC2")
+            {
+                inticon.SetActive(true);
+                panelNPCHablar.SetActive(true);
 
-            NPCMujer2.GetComponent<Animator>().SetBool("isIdle", true);
-            checkMujer2 = true;
+                NPCMujer2.GetComponent<Animator>().SetBool("isIdle", true);
+                checkMujer2 = true;
+            }
+        }
+
+        if (NPCHombre1Data.hasTalked == false)
+        {
+            if (col.gameObject.tag == "NPC3")
+            {
+                inticon.SetActive(true);
+                panelNPCHablar.SetActive(true);
+            }
+        }
+
+        if (NPCMujer1Data.hasTalked == false)
+        {
+            if (col.gameObject.tag == "NPC4")
+            {
+                inticon.SetActive(true);
+                panelNPCHablar.SetActive(true);
+            }
         }
     }
 
     void OnTriggerStay(Collider col)
     {
-        if (col.gameObject.tag == "NPC")
+        if (NPCOddChap.hasTalked == false)
         {
-            if (check == false)
+            if (col.gameObject.tag == "NPC")
             {
+                if (check == false)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        HmmNPC.Play();
+                        Player.GetComponent<FirstPersonController>().enabled = false;
+                        inticon.SetActive(false);
+                        panelNPCHablar.SetActive(false);
+                        panelNPCMision.SetActive(true);
+                        StartCoroutine(HablarConNPC1());
+                        check = true;
+                        NPCOddChap.hasTalked = true;
+                    }
+                }
+
+                if (LogicaObjetivos.check3 == true)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        HmmNPC.Play();
+                        Player.GetComponent<FirstPersonController>().enabled = false;
+                        inticon.SetActive(false);
+                        panelNPCHablar.SetActive(false);
+                        PanelGeneral.SetActive(false);
+                        panelNPCMision.SetActive(true);
+                        StartCoroutine(HablarConNPC2());
+                    }
+                }
+            }
+        }
+        
+        if (NPCMujer2Data.hasTalked == false)
+        {
+            if (col.gameObject.tag == "NPC2")
+            {
+                NPCMujer2.GetComponent<Animator>().SetBool("isIdle", true);
+                checkMujer2 = true;
+
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     HmmNPC.Play();
                     Player.GetComponent<FirstPersonController>().enabled = false;
                     inticon.SetActive(false);
                     panelNPCHablar.SetActive(false);
+                    PanelGeneral.SetActive(false);
                     panelNPCMision.SetActive(true);
-                    StartCoroutine(HablarConNPC1());
-                    check = true;
+                    StartCoroutine(HablarConNPC3());
+                    NPCMujer2Data.hasTalked = true;
                 }
             }
-
-            if (LogicaObjetivos.check3 == true)
+        }
+        
+        if (NPCHombre1Data.hasTalked == false)
+        {
+            if (col.gameObject.tag == "NPC3")
             {
                 if (Input.GetKeyDown(KeyCode.E))
                 {
@@ -91,28 +159,27 @@ public class LogicaNPC : MonoBehaviour
                     panelNPCHablar.SetActive(false);
                     PanelGeneral.SetActive(false);
                     panelNPCMision.SetActive(true);
-                    StartCoroutine(HablarConNPC2());
+                    StartCoroutine(HablarConNPC4());
+                    NPCHombre1Data.hasTalked = true;
                 }
             }
         }
 
-        if (col.gameObject.tag == "NPC2")
+        if (NPCMujer1Data.hasTalked == false)
         {
-            inticon.SetActive(true);
-            panelNPCHablar.SetActive(true);
-
-            NPCMujer2.GetComponent<Animator>().SetBool("isIdle", true);
-            checkMujer2 = true;
-
-            if (Input.GetKeyDown(KeyCode.E))
+            if (col.gameObject.tag == "NPC4")
             {
-                HmmNPC.Play();
-                Player.GetComponent<FirstPersonController>().enabled = false;
-                inticon.SetActive(false);
-                panelNPCHablar.SetActive(false);
-                PanelGeneral.SetActive(false);
-                panelNPCMision.SetActive(true);
-                StartCoroutine(HablarConNPC2());
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    HmmNPC.Play();
+                    Player.GetComponent<FirstPersonController>().enabled = false;
+                    inticon.SetActive(false);
+                    panelNPCHablar.SetActive(false);
+                    PanelGeneral.SetActive(false);
+                    panelNPCMision.SetActive(true);
+                    StartCoroutine(HablarConNPC5());
+                    NPCMujer1Data.hasTalked = true;
+                }
             }
         }
     }
@@ -133,19 +200,40 @@ public class LogicaNPC : MonoBehaviour
             NPCMujer2.GetComponent<Animator>().SetBool("isIdle", false);
             checkMujer2 = false;
         }
+
+        if (col.gameObject.tag == "NPC3")
+        {
+            inticon.SetActive(false);
+            panelNPCHablar.SetActive(false);
+        }
+
+        if (col.gameObject.tag == "NPC4")
+        {
+            inticon.SetActive(false);
+            panelNPCHablar.SetActive(false);
+        }
     }
 
     IEnumerator HablarConNPC1()
     {
-        textoObjetivoNPC.text = "Odd Chap:  Que  laboratorios  tan  aburridos!";
+        textoObjetivoNPC.text = NPCOddChap.Dialogo1;
         yield return new WaitForSeconds(2.5f);
-        textoObjetivoNPC.text = "Odd Chap:  Si  tan  solo  se  hiciera  una  fiesta...";
+        textoObjetivoNPC.text = NPCOddChap.Dialogo2;
         yield return new WaitForSeconds(2.5f);
-        textoObjetivoNPC.text = "Odd Chap:  Crees  que  se  pueda?";
+        textoObjetivoNPC.text = NPCOddChap.Dialogo3;
         yield return new WaitForSeconds(2.5f);
-        textoObjetivoNPC.text = "Odd Chap:  Genial, manos a  la  obra!";
+        textoObjetivoNPC.text = NPCOddChap.Dialogo4;
         yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCOddChap.Dialogo5;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCOddChap.Dialogo6;
+        yield return new WaitForSeconds(2.5f);
+
         panelNPCMision.SetActive(false);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+        NPCOddChap.hasTalked = true;
+
+        /*panelNPCMision.SetActive(false);
         Player.GetComponent<FirstPersonController>().enabled = true;
         MissionObjects.SetActive(true);
         PanelGeneral.SetActive(true);
@@ -153,7 +241,7 @@ public class LogicaNPC : MonoBehaviour
         RelaxingMusic.Stop();
         DiscoMusic.Play();
         check3 = true;
-        panelTimer.SetActive(true);
+        panelTimer.SetActive(true);*/
     }
 
     IEnumerator HablarConNPC2()
@@ -162,5 +250,51 @@ public class LogicaNPC : MonoBehaviour
         yield return new WaitForSeconds(2.5f);
         Player.GetComponent<FirstPersonController>().enabled = true;
         check4 = true;
+    }
+
+    IEnumerator HablarConNPC3()
+    {
+        textoObjetivoNPC.text = NPCMujer2Data.Dialogo1;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer2Data.Dialogo2;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer2Data.Dialogo3;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer2Data.Dialogo4;
+        yield return new WaitForSeconds(2.5f);
+        panelNPCMision.SetActive(false);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    IEnumerator HablarConNPC4()
+    {
+        textoObjetivoNPC.text = NPCHombre1Data.Dialogo1;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCHombre1Data.Dialogo2;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCHombre1Data.Dialogo3;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCHombre1Data.Dialogo4;
+        yield return new WaitForSeconds(2.5f);
+        panelNPCMision.SetActive(false);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    IEnumerator HablarConNPC5()
+    {
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo1;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo2;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo3;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo4;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo5;
+        yield return new WaitForSeconds(2.5f);
+        textoObjetivoNPC.text = NPCMujer1Data.Dialogo6;
+        yield return new WaitForSeconds(2.5f);
+        panelNPCMision.SetActive(false);
+        Player.GetComponent<FirstPersonController>().enabled = true;
     }
 }
---
+++ Assets/ScriptableObjects/NPC1Data.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New NPC1Data", menuName = "NPC1Data")]
+
+public class NPC1Data : ScriptableObject
+{
+    [SerializeField] private string dialogo1, dialogo2, dialogo3, dialogo4, dialogo5, dialogo6;
+   public bool hasTalked;
+
+    public string Dialogo1 { get { return dialogo1; } }
+    public string Dialogo2 { get { return dialogo2; } }
+    public string Dialogo3 { get { return dialogo3; } }
+    public string Dialogo4 { get { return dialogo4; } }
+    public string Dialogo5 { get { return dialogo5; } }
+    public string Dialogo6 { get { return dialogo6; } }
+}
